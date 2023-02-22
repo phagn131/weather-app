436621eb29b6b722eb911951c74bd3c03d24da46
phagn131 <>
2023-02-22 17:07:50 -0500

    DOM elements init, card init in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
-// setup 
+// save reference to important DOM elements
+var searchFormEl = document.querySelector('#city-search');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchFormEl= document.querySelector('#city-search').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchFormEl) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  var queryString = './search-results.html?q=' + searchFormEl + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
