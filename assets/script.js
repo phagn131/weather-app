9486eca641cb34388adf71c196c41164b1974ba1
phagn131 <>
2023-02-23 14:55:34 -0500

    setup tasks for finishing js

--- assets/script.js
+++ assets/script.js
@@ -16,16 +16,26 @@ var apiKey = $('8c51fe51c4de669f61dd9394932b043e')
 
 function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = '8c51fe51c4de669f61dd9394932b043e';
+    var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
   
+    // add lat and long
+
     fetch(requestUrl)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
         console.log(data);
+      })};
 
-        var currentDateTime = moment();
-// .text(currentDateTime.format("MMM Do, YYYY"));
+    // return repsonse 
 
-}};
+      tempEl.text
+
+    // Add all elements
+    
+    // commit to local storage for search history button
+
+    // Render cards with 5-day forecast after retreiving data
+
+    
