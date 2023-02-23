a75ff8b624d10f74fceb7024ce7f71374ce51c3a
phagn131 <>
2023-02-23 18:29:04 -0500

    added lat and lon, openweather data

--- assets/script.js
+++ assets/script.js
@@ -15,29 +15,42 @@ var fiveDayEl = $('#fiveDayForecast');
 var apiKey = '8c51fe51c4de669f61dd9394932b043e'
 
 function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=Houston&appid=" + apiKey;
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=Houston&appid=" + apiKey;
 
-    
-  
-    // add lat and long
+  // add lat and long
 
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      })};
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      getCityWeather(lat, lon, cityData)
+    })
+};
 
-      getApi();
+function getCityWeather(lat, lon, cityData) {
+  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=" + apiKey;
 
-    // return repsonse 
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+};
 
-      tempEl.text
+getApi();
 
-    // Add all elements
-    
-    // commit to local storage for search history button
+// return repsonse
 
+//tempEl.text
+
+// Add all elements
+
+// commit to local storage for search history button
+//localStorage.setItem();
     // Render cards with 5-day forecast after retreiving data
+
