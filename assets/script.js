17b76ef0a34b98d01edcd7b9da75182cc878ad72
phagn131 <>
2023-02-25 15:03:20 -0500

    fix api path, add function to get data from api, add on click, update html for data

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 // save reference to important DOM elements
-var searchCityEl = $('#searchCity');
+var CityEl = $('#searchCity');
 var onClickEl = $('#searchButton');
 var searchCityEl = $('#search');
 var searchHistoryEl = $('#searchHistory');
 var searchContainerEl = $('#searchContainer');
 var currentWeatherEl = $('#currentWeather');
-var searchCityEl = $('#cityName');
-var searchCityEl = $('#weatherInfo');
+// var searchCityEl = $('#cityName');
+// var searchCityEl = $('#weatherInfo');
 var tempEl = $('#currentTemp');
 var humidityEl = $('#currentHumidity');
 var windEl = $('#currentWind');
@@ -14,9 +14,9 @@ var futureForecastEl = $('#futureForecast');
 var fiveDayEl = $('#fiveDayForecast');
 var apiKey = '8c51fe51c4de669f61dd9394932b043e'
 
-function getApi() {
+function fiveDayForecast(cityData) {
   // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=Houston&appid=" + apiKey;
+  var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityData + "&units=imperial&appid=" + apiKey;
 
   // add lat and long
 
@@ -26,12 +26,38 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
-      getCityWeather(lat, lon, cityData)
+      $("#day1").html("city: " + data.city.name)
+      $("#day1Temp").html("temp: " + data.list[1].main.temp + " F")
+      $("#day1Wind").html("wind: " + data.list[1].wind.speed + " MPH")
+      $("#day1Humidity").html("humidity: " + data.list[1].main.humidity)
+
+      $("#day2").html("city: " + data.city.name)
+      $("#day2Temp").html("temp: " + data.list[2].main.temp + " F")
+      $("#day2Wind").html("wind: " + data.list[2].wind.speed + " MPH")
+      $("#day2Humidity").html("humidity: " + data.list[2].main.humidity)
+
+      $("#day3").html("city: " + data.city.name)
+      $("#day3Temp").html("temp: " + data.list[3].main.temp + " F")
+      $("#day3Wind").html("wind: " + data.list[3].wind.speed + " MPH")
+      $("#day3Humidity").html("humidity: " + data.list[3].main.humidity)
+
+      $("#day4").html("city: " + data.city.name)
+      $("#day4Temp").html("temp: " + data.list[4].main.temp + " F")
+      $("#day4Wind").html("wind: " + data.list[4].wind.speed + " MPH")
+      $("#day4Humidity").html("humidity: " + data.list[4].main.humidity)
+
+      $("#day5").html("city: " + data.city.name)
+      $("#day5Temp").html("temp: " + data.list[5].main.temp + " F")
+      $("#day5Wind").html("wind: " + data.list[5].wind.speed + " MPH")
+      $("#day5Humidity").html("humidity: " + data.list[5].main.humidity)
+
+      
+      
     })
 };
 
-function getCityWeather(lat, lon, cityData) {
-  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=" + apiKey;
+function getCityWeather(cityData) {
+  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityData + "&units=imperial&appid=" + apiKey;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -39,10 +65,21 @@ function getCityWeather(lat, lon, cityData) {
     })
     .then(function (data) {
       console.log(data);
+      $("#cityName").html("Current weather for: " + data.name)
+      $("#currentTemp").html("temp: " + data.main.temp)
+      $("#currentHumidity").html("humidity: " + data.main.humidity)
+      $("#currentWind").html("wind: " + data.wind.speed)
+      fiveDayForecast(cityData)
     })
 };
 
-getApi();
+onClickEl.click(function (event) {
+
+  var city = CityEl.val()
+  console.log(city);
+  getCityWeather(city)
+}) 
+
 
 // return repsonse
 
